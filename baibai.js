e2ea655a7fbbe2870bd6bfead98efa9adcb3c719
liubin <1433917529@qq.com>
2020-03-17 17:37:10 +0800

    修正mainOC

--- baibai.js
+++ baibai.js
@@ -1,4 +1,27 @@
 $(function(){
+  
+  var $btn = $('#testpass'),
+      num = 60,
+      timer;
+
+  $btn.click(function(){
+    alert('验证码已发送，请注意查收！！！')
+    //定时器的时间响应
+     timer = setInterval(function(){
+     num--;
+     if(num === -1){
+       clearInterval(timer);
+       $btn.val('验证码');
+       $btn.removeAttr('disabled');
+     }else{
+       $btn.attr("disabled",true);
+       $btn.val('验证码(' + num + 's)');
+     }
+   },1000);
+
+  });
+
+
   var t,
       a = $('input[type="button"]')
 
